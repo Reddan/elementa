1272410f162aee60224b791bde9ba09a225314d6
Hampus Hallman <hampus@hallman.eu>
2026-01-05 11:13:29 +0100

    deduped/batches improvements

--- src/utils/deduped.ts
+++ src/utils/deduped.ts
@@ -1,5 +1,5 @@
 import hash from "object-hash"
-import {zip} from "./utils"
+import {assert, zip} from "./utils"
 
 type Options = {
   cache?: boolean
@@ -7,74 +7,56 @@ type Options = {
   timeout?: number
 }
 
-export const dedupedBatch = <T, U>(callback: (inputs: T[]) => Promise<U[]>, options: Options = {}) => {
-  type Resolve = (output: U) => void
-  type Reject = (reason?: any) => void
+export function dedupedBatch<T, U>(callback: (inputs: T[]) => Promise<U[]>, options: Options = {}) {
   const {cache = false, size, timeout} = options
   const inputByHash = new Map<string, T>()
-  const onOutputByHash: Record<string, [Resolve, Reject][]> = {}
-  const outputByHash: Record<string, U> = {}
-  let handleBulkTimeout: number
+  const outputByHash = new Map<string, U>()
+  const onOutputByHash: Record<string, [(output: U) => void, (reason?: any) => void][]> = {}
+  let batchTimeout: number
+
+  const submit = () => {
+    const hashes = inputByHash.keys().toArray()
+    const inputs = inputByHash.values().toArray()
+    inputByHash.clear()
+    callback(inputs).then(outputs => {
+      for (const [valueHash, output] of zip(hashes, outputs)) {
+        if (cache) {
+          outputByHash.set(valueHash, output)
+        }
+        onOutputByHash[valueHash]!.forEach(([resolve]) => resolve(output))
+        delete onOutputByHash[valueHash]
+      }
+    }).catch(err => {
+      for (const valueHash of hashes) {
+        onOutputByHash[valueHash]!.forEach(([, reject]) => reject(err))
+        delete onOutputByHash[valueHash]
+      }
+    })
+  }
 
   return (input: T) => new Promise<U>((resolve, reject) => {
     const valueHash = hash(input ?? null)
-    if (Object.hasOwn(outputByHash, valueHash)) {
-      return resolve(outputByHash[valueHash]!)
+    if (outputByHash.has(valueHash)) {
+      return resolve(outputByHash.get(valueHash)!)
     }
-    if (!onOutputByHash[valueHash]) {
-      inputByHash.set(valueHash, input)
-      clearTimeout(handleBulkTimeout)
-      const hashes = inputByHash.keys().toArray()
-      const inputs = inputByHash.values().toArray()
-      handleBulkTimeout = setTimeout(() => {
-        hashes.forEach(valueHash => inputByHash.delete(valueHash))
-        void callback(inputs).then(outputs => {
-          for (const [valueHash, output] of zip(hashes, outputs)) {
-            if (cache) {
-              outputByHash[valueHash] = output
-            }
-            onOutputByHash[valueHash]!.forEach(([resolve]) => resolve(output))
-            delete onOutputByHash[valueHash]
-          }
-        }).catch(err => {
-          for (const valueHash of hashes) {
-            onOutputByHash[valueHash]!.forEach(([, reject]) => reject(err))
-            delete onOutputByHash[valueHash]
-          }
-        })
-      }, timeout)
-      if (size && inputByHash.size === size) {
-        handleBulkTimeout = 0
-        inputByHash.clear()
-      }
-      onOutputByHash[valueHash] = []
+    const onOutput = onOutputByHash[valueHash] ??= []
+    onOutput.push([resolve, reject])
+    if (onOutput.length > 1) return
+    inputByHash.set(valueHash, input)
+    clearTimeout(batchTimeout)
+    if (size && inputByHash.size >= size) {
+      submit()
+    } else {
+      batchTimeout = setTimeout(submit, timeout)
     }
-    onOutputByHash[valueHash]!.push([resolve, reject])
   })
 }
 
-export const deduped = <T extends any[], U>(callback: (...input: T) => Promise<U>) => {
-  type Resolve = (output: U) => void
-  type Reject = (reason?: any) => void
-  const onOutputByHash: Record<string, [Resolve, Reject][]> = {}
-  const outputByHash: Record<string, U> = {}
+export function deduped<T extends any[], U>(callback: (...input: T) => Promise<U>, options: Omit<Options, "size"> = {}) {
+  const batched = dedupedBatch(async (inputs: T[]): Promise<U[]> => {
+    assert(inputs.length === 1)
+    return [await callback(...inputs[0]!)]
+  }, {cache: true, ...options, size: 1})
 
-  return (...input: T) => new Promise<U>((resolve, reject) => {
-    const valueHash = hash(input)
-    if (Object.hasOwn(outputByHash, valueHash)) {
-      return resolve(outputByHash[valueHash]!)
-    }
-    if (!onOutputByHash[valueHash]) {
-      callback(...input).then(output => {
-        outputByHash[valueHash] = output
-        onOutputByHash[valueHash]!.forEach(([resolve]) => resolve(output))
-        delete onOutputByHash[valueHash]
-      }).catch(err => {
-        onOutputByHash[valueHash]!.forEach(([, reject]) => reject(err))
-        delete onOutputByHash[valueHash]
-      })
-      onOutputByHash[valueHash] = []
-    }
-    onOutputByHash[valueHash]!.push([resolve, reject])
-  })
+  return (...input: T) => batched(input)
 }
